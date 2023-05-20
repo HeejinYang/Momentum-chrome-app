88f62574f4c686d2bd67a05cf8c5c62f474c5758
Heejin <huren091@naver.com>
2023-05-21 00:31:45 +0900

    weather 완성

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,25 @@
 function onGeoSuccess(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log(`당신의 위치 \n위도:${lat}, 경도:${lng}`);
+    const lon = position.coords.longitude;
+    const API_KEY = "";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+    // 자바스크립트가 직접 url을 부른다
+    // fetch는 promise, 시간이 좀 걸리고 일어난다?
+    fetch(url)
+    .then((response) => response.json())
+    .then((data)=>{
+        const city = document.querySelector("#weather span:nth-child(1)");
+        const weather = document.querySelector("#weather span:nth-child(2)");
+        const temp = document.querySelector("#weather span:nth-child(3)");
+        city.innerHTML = `위치: ${data.name}`;
+        weather.innerHTML = `날씨: ${data.weather[0].main}`;
+        temp.innerHTML = `온도: ${data.main.temp}`;
+        
+
+        
+
+    });
 }
 
 function onGeoFail(){
